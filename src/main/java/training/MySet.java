8f91d4958db6e159d7c47b10537e0b97086a6bc7
Stefan Knies <knies@plansysteme.de>
2023-07-17 18:38:36 +0200

    4 of 5 tests pass, testLarge fail?

--- src/main/java/training/MySet.java
+++ src/main/java/training/MySet.java
@@ -1,5 +1,8 @@
 package training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simplified (and non-generic) implementation of <code>java.util.Set&lt;E&gt;</code>.
  * Stores <code>Object</code> and makes sure that all <code>Object</code>
@@ -7,56 +10,66 @@ package training;
  * <code>Object</code> equality is based on <code>Object.equals()</code>.
  */
 public class MySet {
+    private List<Object> objectList = new ArrayList<>();
 
     public MySet() {
         // constructor implementation
     }
-    
+
     public MySet(Object[] initialElements) {
         // constructor implementation
+
     }
 
     /**
      * Add <code>obj</code> to the set. If <code>obj</code> is already
      * in set, nothing happens.
+     *
      * @param obj The <code>Object</code> to be added.
      */
     public void add(Object obj) {
-        return;
+        objectList.add(obj);
     }
 
     /**
-     * Removes <code>obj</code> from the set, if it exists. 
+     * Removes <code>obj</code> from the set, if it exists.
      * If the set does not contain <code>obj</code>, nothing happens.
+     *
      * @param obj The <code>Object</code> to be removed.
      */
     public void remove(Object obj) {
-        return;
+        objectList.remove(obj);
     }
 
     /**
      * Checks if the set contains a given element.
+     *
      * @param obj Element to be checked.
-     * @return Returns <code>true</code> if this set contains the given element. 
+     * @return Returns <code>true</code> if this set contains the given element.
      * <code>False</code> otherwise.
      */
     public boolean contains(Object obj) {
-        return false;
+        if (objectList.contains(obj)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * Returns the number of elements in set.
      */
     public int size() {
-        return 0;
+        return objectList.size();
     }
 
     /**
      * Returns whether this set does not contain any elements.
+     *
      * @return <code>true</code> if there are no elements
      */
     public boolean isEmpty() {
-        return true;
+        return objectList.isEmpty();
     }
 
 }
